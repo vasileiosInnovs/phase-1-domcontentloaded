0c4b6dd2876d7647761d06027ef800bdd884c931
hagee <hagee.ktr@outlook.com>
2025-03-14 22:37:43 +0300

    Change text when content is loaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const textReplace = document.querySelector('p');
+    textReplace.textContent = "This is really cool!";
+}
+)
+
